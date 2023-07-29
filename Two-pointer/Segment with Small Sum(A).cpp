bf4e0adbe85fc1e09a7e5ab27768201d08ea7323
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2023-07-30 00:24:22 +0530

    Create Segment with Small Sum(A).cpp

---
+++ Two-pointer/Segment with Small Sum(A).cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ull n;
+    
+    ull s;
+    cin>>n>>s;
+    
+    vector<ull>a(n);
+    
+    for(ull i=0;i<n;i++)
+    {
+      cin>>a[i];  
+    }
+    
+    ull sum = 0;
+    ull r=0;
+    ull l=0;
+    ull res = 0;
+    for(r=0;r<n;r++)
+    {
+        sum += a[r];
+        
+        while(sum>s)
+        {
+            sum -= a[l];
+            l++;
+        }
+        
+        res = max(res,r-l+1);
+        
+    }
+    cout<<res;
+	
+}
