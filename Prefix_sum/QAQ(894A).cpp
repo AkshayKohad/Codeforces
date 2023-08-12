73504ddca4485b73a46eb435fabe47fc732b4cc6
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2023-08-12 15:06:49 +0530

    Create QAQ(894A).cpp

---
+++ Prefix_sum/QAQ(894A).cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    string a;
+    cin>>a;
+    
+    ll n = a.length();
+    vector<ll>prefix_sum_of_q(n+1,0);
+    
+    
+    for(int i=1;i<=n;i++)
+    {
+        if(a[i-1]=='Q')
+        prefix_sum_of_q[i] = prefix_sum_of_q[i-1] + 1;
+        
+        else
+        prefix_sum_of_q[i] = prefix_sum_of_q[i-1];
+    }
+  
+    ll result = 0;
+    
+    for(ll i=1;i<=n;i++)
+    {
+        if(a[i-1]=='A')
+        {
+            
+            result += prefix_sum_of_q[i-1]*(prefix_sum_of_q[n]-prefix_sum_of_q[i]);
+        }
+    }
+    
+    cout<<result;
+    
+}
