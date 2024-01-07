5ce0c0665dae442852f46a3f1e846036bfbf240d
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2024-01-08 00:40:24 +0530

    Update Good Numbers (hard version)(1249C2).cpp

--- Maths/Good Numbers (hard version)(1249C2).cpp
+++ Maths/Good Numbers (hard version)(1249C2).cpp
@@ -1,3 +1,9 @@
+Let's see the representation of n in the ternary numeral system. If it has no twos, then the answer is n. Otherwise, let pos2
+be the maximum position of 2 in the ternary representation. Then we obviously need to replace it with 0 and add some power of three to the right from it. Let pos0
+be the leftmost position of 0 to the right from pos2. We can add 3 pos0 and replace all digits from the position pos0−1 to the position 0
+with 0. Then the resulting number will be good because we replaced all twos with zeros and the minimum because, in fact, we added only one power of three 
+and this power is the minimum one we could add.    Time complexity is O(logn) per query.
+
 #include <bits/stdc++.h>
 using namespace std;
 
