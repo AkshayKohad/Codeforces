2bf249ae6601c5f8b5b27d058e8e9635e786dda5
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-07-26 21:17:29 +0530

    Create Blackboard Game(2123A).java

---
+++ Game Theory/Blackboard Game(2123A).java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int t = sc.nextInt();
+    
+    while(t-- > 0){
+        int n = sc.nextInt();
+        
+        if(n%4==0)System.out.println("BOB");
+        else System.out.println("ALICE");
+    }
+  }
+}
