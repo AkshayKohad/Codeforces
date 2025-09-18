28b997ef2dfced556969451a875559c7669dd2ed
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-09-19 00:54:38 +0530

    Create Game With Sticks(451A).cpp

---
+++ Game Theory/Game With Sticks(451A).cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int mini = min(n,m);
+    
+    if(mini%2)cout<<"Akshat"<<endl;
+    else cout<<"Malvika"<<endl;
+    
+    return 0;
+}
