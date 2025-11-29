58464f70019910c835c19b0581d3fa10e261a4d0
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-11-29 18:52:12 +0530

    Create Coins(1814A).cpp

---
+++ Maths/Coins(1814A).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        ll n,k;
+        
+        cin>>n>>k;
+        
+        if(n%2==0){
+            cout<<"YES"<<endl;
+        }else{
+            if(k%2==1 && k<=n){
+                cout<<"YES"<<endl;
+            }else{
+                cout<<"NO"<<endl;
+            }
+        }
+        
+    }
+}
